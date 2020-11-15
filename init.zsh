5b18ca2450b918ead77e870409580a6cf8129be7
Brandon Zarrella <zarrellab@gmail.com>
2020-11-15 15:12:44 -0700

    use zprezto files for znvm

--- init.zsh
+++ init.zsh
@@ -1,19 +1,8 @@
 #! /bin/zsh 
 
-# ensure znvm download location is correct
-local ZNVM_PATH="$HOME/.znvm"
-local ZNVMSH_PATH="$ZNVM_PATH/znvm.sh"
-
-if [ ! -f $ZNVMSH_PATH ]; then
-	echo "ERROR: could not find $ZNVMSH_PATH. Please ensure $ZNVM_PATH exists."
-	echo "INFO: follow installation instructions as stated in the README.md file."
-	echo "Aborted."
-	exit 1
-fi
-
 #load main script and assign nvm alias
-source "$ZNVM_PATH/loadnvmrc.sh"
-source "$ZNVM_PATH/znvm.conf.sh"
+source "$HOME/.zprezto/modules/zsh/znvm/loadnvmrc.sh"
+source "$HOME/.zprezto/modules/zsh/znvm/znvm.conf.sh"
 
 znvm_contains_elem () {
   local e match="$1"
